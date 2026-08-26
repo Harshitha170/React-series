d5202683f441b9b3843978e0342d154125fdb5c7
Harshitha170 <jharshitha28@gmail.com>
2026-08-26 14:45:25 +0530

    feat: initialize redux toolkit setup with todo functionality
    - Added icons.svg for SVG icons used in the application.
    - Created App.css for styling the main application.
    - Implemented App.jsx as the main application component with AddTodo and Todos components.
    - Developed AddTodo component for adding new todos with input handling.
    - Created Todos component to display the list of todos and allow removal.
    - Set up Redux store in store.js to manage todo state.
    - Added initial todo slice with add and remove reducers in todoSlice.js.
    - Included hero.png and react.svg assets for the application.
    - Configured Vite with React and Tailwind CSS support in vite.config.js.
    - Added index.css to import Tailwind CSS styles.
    - Updated main.jsx to render the application with Redux Provider.

---
+++ reduxToolkit/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+
+import './App.css'
+import AddTodo from './Components/AddTodo'
+import Todos from './Components/Todos'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+   <h1>Learn aboutt redux toolkit</h1>
+   <AddTodo />
+   <Todos />
+    </>
+  )
+}
+
+export default App
